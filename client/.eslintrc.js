0e9eebf04f86a5cc8c57edcba48cb05f06b5a81c
Amy Qiu <qiuyingquan@gmail.com>
2019-10-09 15:43:01 -0400

    Set up rules for lint

---
+++ client/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'extends': ['airbnb', 'prettier'],
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/no-unused-state': 'off',
+    'react/no-unused-prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'prettier/prettier': 'error',
+  },
+  'globals': {
+    "fetch": false
+  },
+  'plugins': ['prettier'],
+}
--- client/App.js
+++ client/App.js
@@ -1,18 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import NewEntryPage from './NewEntryPage';
+// @flow
+import React from "react";
+import NewEntryPage from "./NewEntryPage";
 
 export default function App() {
-  return (
-    <NewEntryPage/>
-  );
+  return <NewEntryPage />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
