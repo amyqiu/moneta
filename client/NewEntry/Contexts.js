6f2b7948d27efc195b050b6f9b4d6c95a2d54692
Amy Qiu <qiuyingquan@gmail.com>
2019-10-16 18:21:08 -0400

    Style cards in new entry page

---
+++ client/NewEntry/Contexts.js
@@ -0,0 +1,15 @@
+const CONTEXTS = [
+  "Alone",
+  "Loud/Busy Environment",
+  "Quiet Environment",
+  "Family/Visitors",
+  "Bathing",
+  "Eating/Drinking",
+  "Behavioural Medication",
+  "Pain Medication",
+  "Treatment",
+  "Directed at Residents",
+  "Directed at Staff"
+];
+
+export default CONTEXTS;
--- client/NewEntry/NewEntryPage.js
+++ client/NewEntry/NewEntryPage.js
@@ -1,16 +1,23 @@
 // @flow
 import * as React from "react";
-import { View, TextInput, ScrollView } from "react-native";
-import { Card, Text, Header } from "react-native-elements";
+import {
+  View,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView
+} from "react-native";
+import { Card, Text, Header, CheckBox } from "react-native-elements";
 import BehaviourCheckbox from "./BehaviourCheckbox";
 import BEHAVIOURS from "./Behaviours";
+import CONTEXTS from "./Contexts";
+import LOCATIONS from "./Locations";
 
 type Props = {};
 
 type State = {
   checkedBehaviours: Map<string, Set<string>>,
-  locations: Set<string>,
-  context: Set<string>,
+  checkedLocations: Set<string>,
+  checkedContexts: Set<string>,
   comments: string,
   time: Date
 };
@@ -20,8 +27,8 @@ export default class NewEntryPage extends React.Component<Props, State> {
     super(props);
     this.state = {
       checkedBehaviours: new Map(),
-      locations: new Set(),
-      context: new Set(),
+      checkedLocations: new Set(),
+      checkedContexts: new Set(),
       comments: "",
       time: new Date()
     };
@@ -43,60 +50,146 @@ export default class NewEntryPage extends React.Component<Props, State> {
     this.setState({ checkedBehaviours });
   };
 
+  handleLocationChecked = (location: string) => {
+    const { checkedLocations } = this.state;
+
+    if (checkedLocations.has(location)) {
+      checkedLocations.delete(location);
+    } else {
+      checkedLocations.add(location);
+    }
+
+    this.setState({ checkedLocations });
+  };
+
+  handleContextChecked = (context: string) => {
+    const { checkedContexts } = this.state;
+
+    if (checkedContexts.has(context)) {
+      checkedContexts.delete(context);
+    } else {
+      checkedContexts.add(context);
+    }
+
+    this.setState({ checkedContexts });
+  };
+
   render() {
-    const { comments } = this.state;
+    const { comments, checkedLocations, checkedContexts } = this.state;
 
     return (
-      <ScrollView>
-        <View style={{ flex: 1 }}>
-          <Header
-            backgroundColor="#808080"
-            placement="left"
-            leftComponent={{
-              icon: "arrow-left",
-              type: "feather",
-              color: "#fff"
-            }}
-            centerComponent={{
-              text: "New Entry",
-              style: { color: "#fff" }
-            }}
-          />
-          <Card>
-            <View>
-              <Text h4>Behaviours Observed</Text>
-              {BEHAVIOURS.map(behaviour => (
-                <BehaviourCheckbox
-                  key={behaviour.label}
-                  label={behaviour.label}
-                  color={behaviour.color}
-                  subBehaviours={behaviour.subBehaviours}
-                  onBehaviourChecked={this.onBehaviourChecked}
+      <KeyboardAvoidingView behavior="position">
+        <ScrollView>
+          <View
+            style={{ flex: 1, backgroundColor: "#f4f4f4", paddingBottom: 16 }}
+          >
+            <Header
+              backgroundColor="#393939"
+              placement="left"
+              leftComponent={{
+                icon: "arrow-left",
+                type: "feather",
+                color: "#ffffff"
+              }}
+              centerComponent={{
+                text: "New Entry",
+                style: { color: "#ffffff", fontSize: 18 }
+              }}
+            />
+            <Card containerStyle={{ borderRadius: 4 }}>
+              <View>
+                <Text h4>Behaviours Observed</Text>
+                {BEHAVIOURS.map(behaviour => (
+                  <BehaviourCheckbox
+                    key={behaviour.label}
+                    label={behaviour.label}
+                    color={behaviour.color}
+                    subBehaviours={behaviour.subBehaviours}
+                    onBehaviourChecked={this.onBehaviourChecked}
+                  />
+                ))}
+              </View>
+            </Card>
+            <Card containerStyle={{ borderRadius: 4 }}>
+              <View>
+                <Text h4>Location</Text>
+                {LOCATIONS.map(location => (
+                  <CheckBox
+                    title={location}
+                    key={location}
+                    checked={checkedLocations.has(location)}
+                    onPress={() => this.handleLocationChecked(location)}
+                    containerStyle={{
+                      backgroundColor: "#ffffff",
+                      borderWidth: 0,
+                      padding: 0
+                    }}
+                    textStyle={{
+                      fontSize: 16,
+                      fontWeight: "normal"
+                    }}
+                    iconType="feather"
+                    checkedIcon="check-square"
+                    uncheckedIcon="square"
+                    checkedColor="#525252"
+                    uncheckedColor="#525252"
+                  />
+                ))}
+              </View>
+            </Card>
+            <Card containerStyle={{ borderRadius: 4 }}>
+              <View>
+                <Text h4>Context</Text>
+                {CONTEXTS.map(context => (
+                  <CheckBox
+                    title={context}
+                    key={context}
+                    checked={checkedContexts.has(context)}
+                    onPress={() => this.handleContextChecked(context)}
+                    containerStyle={{
+                      backgroundColor: "#ffffff",
+                      borderWidth: 0,
+                      padding: 0
+                    }}
+                    textStyle={{
+                      fontSize: 16,
+                      fontWeight: "normal"
+                    }}
+                    iconType="feather"
+                    checkedIcon="check-square"
+                    uncheckedIcon="square"
+                    checkedColor="#525252"
+                    uncheckedColor="#525252"
+                  />
+                ))}
+              </View>
+            </Card>
+            <Card containerStyle={{ borderRadius: 4 }}>
+              <View>
+                <Text h4>Comments</Text>
+                <TextInput
+                  style={{
+                    margin: 4,
+                    padding: 8,
+                    height: 64,
+                    borderColor: "#393939",
+                    borderWidth: 1,
+                    borderRadius: 4,
+                    backgroundColor: "#ffffff",
+                    textAlignVertical: "top"
+                  }}
+                  onChangeText={value => this.setState({ comments: value })}
+                  value={comments}
+                  multiline
+                  height={100}
                 />
-              ))}
-            </View>
-          </Card>
-          <Card>
-            <Text h4>Location</Text>
-          </Card>
-          <Card>
-            <Text h4>Context</Text>
-          </Card>
-          <TextInput
-            style={{
-              margin: 15,
-              padding: 8,
-              height: 40,
-              borderColor: "#d3d3d3",
-              borderWidth: 1
-            }}
-            onChangeText={value => this.setState({ comments: value })}
-            value={comments}
-            multiline
-            height={100}
-          />
-        </View>
-      </ScrollView>
+              </View>
+            </Card>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      // TODO: Make time selector and submit button
     );
   }
 }
