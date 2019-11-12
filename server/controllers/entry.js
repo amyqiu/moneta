e17f2b10ecc225e2fcdd011af40d70a0c14ade5f
Amy Qiu <qiuyingquan@gmail.com>
2019-11-12 13:46:07 -0500

    Add linting for server

--- server/controllers/entry.js
+++ server/controllers/entry.js
@@ -1,12 +1,11 @@
+const { validationResult, body } = require('express-validator/check');
 const Entry = require('../models/entry');
 const Observation = require('../models/observation');
-const Patient = require('../models/patient');
-const { validationResult, body } = require('express-validator/check');
 
 exports.validate = (method) => {
   switch (method) {
     case 'entry_create': {
-     return [
+      return [
         body('patient_ID').exists().isString(),
         body('observation_ID').exists().isString(),
         body('behaviours').optional(),
@@ -14,44 +13,49 @@ exports.validate = (method) => {
         body('contexts').optional(),
         body('comments').optional().isString(),
         body('time').exists().isInt(),
-      ]
+      ];
+    }
+    default: {
+      return [];
     }
   }
-}
+};
 
 // Test url
-exports.entry_test = function (req, res) {
-  return res.status(200).send('Greetings from the entry test controller!');
-};
+exports.entry_test = (req, res) => res.status(200).send('Greetings from the entry test controller!');
 
 function updateAggregatedData(req, res, obs, entry) {
   try {
     // Populate aggregated behaviour data
-    for (behaviour of Object.keys(req.body.behaviours)) {
-      if (!obs.aggregated_behaviours.has(behaviour)){
-        return res.status(500).send("Invalid behaviour specified: " + behaviour);
+    for (let i = 0; i < Object.keys(req.body.behaviours).length; i += 1) {
+      const behaviour = Object.keys(req.body.behaviours)[i];
+      if (!obs.aggregated_behaviours.has(behaviour)) {
+        return res.status(500).send(`Invalid behaviour specified: ${behaviour}`);
       }
       obs.aggregated_behaviours.get(behaviour).push(1);
-      for (subbehaviour of req.body.behaviours[behaviour]) {
-        if (!obs.aggregated_behaviours.has(subbehaviour)){
-          return res.status(500).send("Invalid sub-behaviour specified");
+      for (let j = 0; j < req.body.behaviours[behaviour].length; j += 1) {
+        const subbehaviour = req.body.behaviours[behaviour][j];
+        if (!obs.aggregated_behaviours.has(subbehaviour)) {
+          return res.status(500).send('Invalid sub-behaviour specified');
         }
         obs.aggregated_behaviours.get(subbehaviour).push(1);
       }
     }
 
     // Populate aggregated contexts data
-    for (context of req.body.contexts) {
-      if (!obs.aggregated_contexts.has(context)){
-        return res.status(500).send("Invalid context specified");
+    for (let i = 0; i < req.body.contexts.length; i += 1) {
+      const context = req.body.contexts[i];
+      if (!obs.aggregated_contexts.has(context)) {
+        return res.status(500).send('Invalid context specified');
       }
       obs.aggregated_contexts.get(context).push(1);
     }
 
     // Populate aggregated locations data
-    for (location of req.body.locations) {
-      if (!obs.aggregated_locations.has(location)){
-        return res.status(500).send("Invalid location specified");
+    for (let i = 0; i < req.body.locations.length; i += 1) {
+      const location = req.body.locations[i];
+      if (!obs.aggregated_locations.has(location)) {
+        return res.status(500).send('Invalid location specified');
       }
       obs.aggregated_locations.get(location).push(1);
     }
@@ -60,27 +64,27 @@ function updateAggregatedData(req, res, obs, entry) {
     obs.entry_times.push(new Date(req.body.time * 1000));
 
     // Add zeroes for all unchecked options
-    obs.aggregated_behaviours.forEach(a => {
+    obs.aggregated_behaviours.forEach((a) => {
       if (a.length < obs.entry_times.length) {
         a.push(0);
       }
     });
-    obs.aggregated_locations.forEach(a => {
+    obs.aggregated_locations.forEach((a) => {
       if (a.length < obs.entry_times.length) {
         a.push(0);
       }
     });
-    obs.aggregated_contexts.forEach(a => {
+    obs.aggregated_contexts.forEach((a) => {
       if (a.length < obs.entry_times.length) {
         a.push(0);
       }
     });
   } catch (e) {
-    return res.status(500).send("An error occurred while aggregating data");
+    return res.status(500).send('An error occurred while aggregating data');
   }
 }
 
-exports.entry_create = function (req, res) {
+exports.entry_create = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
@@ -97,21 +101,21 @@ exports.entry_create = function (req, res) {
         contexts: req.body.contexts,
         comments: req.body.comments,
         time: new Date(req.body.time * 1000),
-      }
+      },
     );
   } catch (createErr) {
     return res.status(500).send(createErr);
   }
 
-  Observation.findById(req.body.observation_ID, function (obsErr, obs) {
+  Observation.findById(req.body.observation_ID, (obsErr, obs) => {
     if (obsErr) {
       return res.status(500).send(obsErr);
-    } else if (!obs) {
-      return res.status(500).send("Observation does not exist");
-    } else if (obs.patient_ID !== req.body.patient_ID) {
-      return res.status(500).send("Invalid patient ID for the observation ID");
-    } else if (obs.end_time != null) {
-      return res.status(500).send("Observation period has ended");
+    } if (!obs) {
+      return res.status(500).send('Observation does not exist');
+    } if (obs.patient_ID !== req.body.patient_ID) {
+      return res.status(500).send('Invalid patient ID for the observation ID');
+    } if (obs.end_time != null) {
+      return res.status(500).send('Observation period has ended');
     }
 
     updateAggregatedData(req, res, obs, entry);
@@ -123,11 +127,11 @@ exports.entry_create = function (req, res) {
       aggregated_locations: obs.aggregated_locations,
       entry_times: obs.entry_times,
     };
-    Observation.findByIdAndUpdate(req.body.observation_ID, {$set: obsUpdate}, function (updateErr) {
+    Observation.findByIdAndUpdate(req.body.observation_ID, { $set: obsUpdate }, (updateErr) => {
       if (updateErr) {
         return res.status(500).send(updateErr);
       }
-      entry.save(function (err) {
+      entry.save((err) => {
         if (err) {
           return res.status(500).send(err);
         }
@@ -137,90 +141,99 @@ exports.entry_create = function (req, res) {
   });
 };
 
-// TODO: find all entries based on the time period
-// We know the
-
-exports.entry_find_all = function (req, res) {
-  Entry.find(function (err, entries) {
+exports.entry_find_all = (req, res) => {
+  Entry.find((err, entries) => {
     if (err) {
       return res.status(500).send(err);
     }
 
     return res.status(200).send(entries);
   });
-}
+};
 
-exports.entry_find_day = function (req, res) {
-  if(isNaN(req.body.month) || isNaN(req.body.day) || isNaN(req.body.year)){
-  return res.status(500).send("Month/Day/Year is not a number");
+exports.entry_find_day = (req, res) => {
+  if (Number.isNaN(req.query.month)
+    || Number.isNaN(req.query.day)
+    || Number.isNaN(req.query.year)) {
+    return res.status(500).send('Month/Day/Year is not a number');
   }
-  console.log("here");
   // use ID of patient to find all of the entries associated with that patient
-  Entry.find({patient_ID: req.body.patient_ID}).
-  exec(function(err,entries){
+  Entry.find({ patient_ID: req.query.patient_ID })
+    .exec((err, entries) => {
+      if (err) {
+        return res.status(500).send(err);
+      } if (!entries) {
+        return res.status(500).send('Entries do not exist');
+      }
+
+      console.log(entries);
+      const month = parseInt(req.query.month, 10);
+      const day = parseInt(req.query.day, 10);
+      const year = parseInt(req.query.year, 10);
+
+      // loop through all of the elements to see if they end up being the thing.
+      const entryDayArray = [];
+      for (let i = 0; i < entries.length; i += 1) {
+        const entry = entries[i];
+        console.log(entry.time);
+        if (entry.time.getMonth() + 1 === month
+          && entry.time.getDate() === day
+          && entry.time.getFullYear() === year) {
+          try {
+            entryDayArray.push(entry);
+          } catch (dayErr) {
+            return res.status(500).send('Error getting entry days from observation');
+          }
+        }
+      }
+      return res.status(200).send(entryDayArray);
+    });
+};
+
+exports.entry_details = (req, res) => {
+  Entry.findById(req.params.id, (err, entry) => {
     if (err) {
       return res.status(500).send(err);
-    } else if (!entries) {
-      return res.status(500).send("Entries does not exist");
-    }
-    // loop through all of the elements to see if they end up being the thing.
-    console.log(entries);
-    entry_day_array = [];
-    for(entry of entries){
-      console.log(entry.time);
-      if (entry.time.getMonth() + 1 == req.body.month && entry.time.getDate() == req.body.day && entry.time.getFullYear() == req.body.year){
-        try{
-        entry_day_array.push(entry)
-      } catch(err){
-        return res.status(500).send("Error getting entry days from observation");
-      }
-      }
-    };
-    return res.status(200).send(entry_day_array);
-  });
-
-}
-
-exports.entry_details = function (req, res) {
-  Entry.findById(req.params.id, function (err, entry) {
-    if (err) {
-      return res.status(500).send(err);
-    } else if (!entry) {
+    } if (!entry) {
       return res.status(500).send('Entry does not exist');
     }
     return res.status(200).send(entry);
   });
 };
 
-exports.entry_update = function (req, res) {
-  Entry.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, entry) {
+exports.entry_update = (req, res) => {
+  Entry.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, entry) => {
     if (err) {
       return res.status(500).send(err);
-    } else if (!entry) {
+    } if (!entry) {
       return res.status(500).send('Entry does not exist');
     }
     return res.status(200).send('Entry updated');
   });
 };
 
-//TODO: delete associated data in aggregated data?
-exports.entry_delete = function (req, res) {
-  Entry.findById(req.params.id, function (err, entry) {
+// TODO: delete associated data in aggregated data?
+exports.entry_delete = (req, res) => {
+  Entry.findById(req.params.id, (err, entry) => {
     if (err) {
       return res.status(500).send(err);
-    } else if (!entry) {
+    } if (!entry) {
       return res.status(500).send('Entry does not exist');
     }
-    Observation.findByIdAndUpdate(entry.observation_ID, { $pullAll: { entries: [entry.id] } }, function (obsErr){
-      if (obsErr) {
-        return res.status(500).send(obsErr);
-      }
-      entry.remove(function (delErr) {
-        if (delErr) {
-          return res.status(500).send(delErr);
+    Observation.findByIdAndUpdate(
+      entry.observation_ID,
+      { $pullAll: { entries: [entry.id] } },
+      (obsErr) => {
+        if (obsErr) {
+          return res.status(500).send(obsErr);
         }
-        return res.status(200).send('Entry deleted successfully');
-      });
-    });
+        entry.remove((delErr) => {
+          if (delErr) {
+            return res.status(500).send(delErr);
+          }
+          return res.status(200).send('Entry deleted successfully');
+        });
+      },
+    );
   });
 };
--- server/controllers/patient.js
+++ server/controllers/patient.js
@@ -1,29 +1,29 @@
-const Patient = require('../models/patient');
-const Observation = require('../models/observation')
 const { validationResult, body } = require('express-validator/check');
+const Patient = require('../models/patient');
+const Observation = require('../models/observation');
 
-//TODO: Add endpoints for getting entries for a day + days with entries
+// TODO: Add endpoints for getting entries for a day + days with entries
 
 exports.validate = (method) => {
   switch (method) {
     case 'patient_create': {
-     return [
+      return [
         body('name').exists().isString(),
         body('age').exists().isInt(),
         body('room').exists().isString(),
         body('profile_picture').exists().isString(),
         body('display_ID').exists().isString(),
-       ]
+      ];
+    } default: {
+      return [];
     }
   }
-}
-
-// Test url
-exports.patient_test = function (req, res) {
-  return res.status(200).send('Greetings from the patient test controller!');
 };
 
-exports.patient_create = function (req, res) {
+// Test url
+exports.patient_test = (req, res) => res.status(200).send('Greetings from the patient test controller!');
+
+exports.patient_create = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
@@ -40,59 +40,59 @@ exports.patient_create = function (req, res) {
         observation_periods: [],
         in_observation: false,
         display_ID: req.body.display_ID,
-      }
+      },
     );
   } catch (createErr) {
     return res.status(500).send(createErr);
   }
 
-  patient.save(function (err) {
+  patient.save((err) => {
     if (err) {
       return res.status(500).send(err);
     }
     return res.status(200).send('Patient created successfully');
-  })
+  });
 };
 
-exports.patient_find_all = function (req, res) {
-  Patient.
-    find().
-    populate('observation_periods', 'start_time end_time').
-    exec(function (err, patients) {
+exports.patient_find_all = (req, res) => {
+  Patient
+    .find()
+    .populate('observation_periods', 'start_time end_time')
+    .exec((err, patients) => {
       if (err) {
         return res.status(500).send(err);
       }
       return res.status(200).send(patients);
     });
-}
+};
 
-exports.patient_details = function (req, res) {
-  Patient.
-    findById(req.params.id).
-    populate('observation_periods', 'start_time end_time').
-    exec(function (err, patient) {
+exports.patient_details = (req, res) => {
+  Patient
+    .findById(req.params.id)
+    .populate('observation_periods', 'start_time end_time')
+    .exec((err, patient) => {
       if (err) {
         return res.status(500).send(err);
-      } else if (!patient) {
-        return res.status(500).send("Patient does not exist");
+      } if (!patient) {
+        return res.status(500).send('Patient does not exist');
       }
       return res.status(200).send(patient);
     });
 };
 
-exports.patient_update = function (req, res) {
-  Patient.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, patient) {
+exports.patient_update = (req, res) => {
+  Patient.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, patient) => {
     if (err) {
       return res.status(500).send(err);
-    } else if (!patient) {
-      return res.status(500).send("Patient does not exist");
+    } if (!patient) {
+      return res.status(500).send('Patient does not exist');
     }
     return res.status(200).send('Patient updated');
   });
 };
 
-exports.patient_delete = function (req, res) {
-  Patient.findByIdAndRemove(req.params.id, function (err) {
+exports.patient_delete = (req, res) => {
+  Patient.findByIdAndRemove(req.params.id, (err) => {
     if (err) {
       return res.status(500).send(err);
     }
@@ -101,60 +101,64 @@ exports.patient_delete = function (req, res) {
 };
 
 // Get days with entries from an observation period
-function get_days_from_period (id, month, year) {
-  return new Promise ((resolve, reject) => {
-    Observation.
-      findById(id).
-      exec(function (err, obs) {
-        var days = [];
-        if (obs.entry_times.length >=  1) {
-          for (time of obs.entry_times) {
-            if (time.getMonth() + 1 == month && time.getFullYear() == year){
-              days.push(time.getDate())
+function getDaysFromPeriod(id, month, year) {
+  return new Promise((resolve) => {
+    Observation
+      .findById(id)
+      .exec((err, obs) => {
+        const days = [];
+        if (obs.entry_times.length >= 1) {
+          for (let i = 0; i < obs.entry_times.length; i += 1) {
+            const time = obs.entry_times[i];
+            if (time.getMonth() + 1 === month && time.getFullYear() === year) {
+              days.push(time.getDate());
             }
           }
         }
-        setTimeout(() => resolve(days), 300)
-  })
-});
+        setTimeout(() => resolve(days), 300);
+      });
+  });
 }
 
 // Get days with entries given month
-exports.get_days_with_entries = function (req, res) {
-  if(isNaN(req.params.month) || isNaN(req.params.year)){
-    return res.status(500).send("Month/Year is not a number");
+exports.find_days_with_entries = (req, res) => {
+  if (Number.isNaN(req.query.month) || Number.isNaN(req.query.year)) {
+    return res.status(500).send('Month/Year is not a number');
   }
-  Patient.
-    findById(req.params.id).
-    populate('observation_periods', 'start_time end_time').
-    exec(async function (err, patient) {
+  Patient
+    .findById(req.query.id)
+    .populate('observation_periods', 'start_time end_time')
+    .exec(async (err, patient) => {
       if (err) {
         return res.status(500).send(err);
-      } else if (!patient) {
-        return res.status(500).send("Patient does not exist");
-      } else if (patient.observation_periods.length < 1){
-        return res.status(500).send("Patient has no observations");
+      } if (!patient) {
+        return res.status(500).send('Patient does not exist');
+      } if (patient.observation_periods.length < 1) {
+        return res.status(500).send('Patient has no observations');
       }
+      const month = parseInt(req.query.month, 10);
+      const year = parseInt(req.query.year, 10);
       // Loop through each observation period, check if start_time is in same month
-      var date_array = [];
-      for (period of patient.observation_periods) {
-        if (period.start_time.getMonth() + 1 == req.params.month && period.start_time.getFullYear() == req.params.year) {
+      const dateArray = [];
+      for (let i = 0; i < patient.observation_periods.length; i += 1) {
+        const period = patient.observation_periods[i];
+        if (period.start_time.getMonth() + 1 === month
+          && period.start_time.getFullYear() === year) {
           try {
-            var days = await get_days_from_period(period.id, req.params.month, req.params.year);
-            date_array.push(days)
-          } catch (err) {
-            return res.status(500).send("Error getting entry days from observation");
+            // eslint-disable-next-line no-await-in-loop
+            const days = await getDaysFromPeriod(period.id, month, year);
+            dateArray.push(days);
+          } catch (daysErr) {
+            return res.status(500).send('Error getting entry days from observation');
           }
         }
-      };
-      // Convert to 1D array and remove duplicates
-      var date_1d = [];
-      for(var i = 0; i < date_array.length; i++) {
-          date_1d = date_1d.concat(date_array[i]);
       }
-      var unique_dates = date_1d.filter(function(elem, index, self) {
-        return index === self.indexOf(elem);
-      })
-      return res.status(200).send(unique_dates);
+      // Convert to 1D array and remove duplicates
+      let date1D = [];
+      for (let i = 0; i < dateArray.length; i += 1) {
+        date1D = date1D.concat(dateArray[i]);
+      }
+      const uniqueDates = date1D.filter((elem, index, self) => index === self.indexOf(elem));
+      return res.status(200).send(uniqueDates);
     });
 };
