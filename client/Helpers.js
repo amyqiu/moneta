cbbcea56267156e21c76558f4361c02d566bbf62
Amy Qiu <qiuyingquan@gmail.com>
2019-12-17 14:39:20 -0500

    Add template for correlations

--- client/Helpers.js
+++ client/Helpers.js
@@ -1,4 +1,8 @@
+import React from "react";
 import { Dimensions } from "react-native";
+import moment from "moment";
+import Icon from "react-native-vector-icons/Ionicons";
+import colours from "./Colours";
 
 export function scaleWidth(percent: number) {
   const deviceWidth = Dimensions.get("window").width;
@@ -9,3 +13,36 @@ export function isTablet() {
   const deviceWidth = Dimensions.get("window").width;
   return deviceWidth > 700;
 }
+
+export function formatObservationDates(observation: Object) {
+  if (observation == null) {
+    return "";
+  }
+  return `${moment(observation.start_time).format("MMM D, YYYY")} - ${moment(
+    observation.end_time || new Date()
+  ).format("MMM D, YYYY")}`;
+}
+
+export function createObservationDropdown(observations: [Object]) {
+  const items = [];
+  observations.forEach(period => {
+    items.push({
+      name: formatObservationDates(period),
+      id: period._id
+    });
+  });
+  return items;
+}
+
+export const SELECT_COLOURS = {
+  primary: colours.primaryGrey,
+  chipColor: colours.primaryGrey
+};
+
+export const SELECT_ICON = (
+  <Icon
+    size={36}
+    name="ios-checkmark"
+    style={{ color: colours.successGreen, marginRight: 16 }}
+  />
+);
